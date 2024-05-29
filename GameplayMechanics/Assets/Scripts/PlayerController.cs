c95375640169fc24ce0457b165cda3971053009f
Jonathan <juarezj2@roverkids.org>
2024-05-29 13:52:35 -0400

    finished 4.3 step 5

--- GameplayMechanics/Assets/Scripts/PlayerController.cs
+++ GameplayMechanics/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     private GameObject focalPoint;
     public bool hasPowerup;
+    private float powerupstrength = 15.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,15 +31,26 @@ public class PlayerController : MonoBehaviour
         {
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
         }
         
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            
             Debug.Log("Collided with" + collision.gameObject.name + "with powerup set to" + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupstrength,ForceMode.Impulse);
         }
     }
 }
